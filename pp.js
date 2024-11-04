47fa75f101573f0b18896934983631c5cc1d2f4a
Brosko123 <ytur123@tutamail.com>
2024-11-04 10:11:13 +0530

    Update pp.js

--- pp.js
+++ pp.js
@@ -1,2 +1,2 @@
 
-"use strict";const DEFAULT_QUALITY=40;export default function pp(req,res,next){let url=req.query.url;if(!url)return res.send("bandwidth-hero-proxy");req.params.url=decodeURIComponent(url);req.params.webp=!req.query.jpeg;req.params.grayscale=req.query.bw!=0;req.params.quality=parseInt(req.query.l,10)||DEFAULT_QUALITY;next()}
+"use strict";const DEFAULT_QUALITY=40;export default function pp(req,res,next){let url=req.query.url;if(!url)return res.send("শুরু");req.params.url=decodeURIComponent(url);req.params.webp=!req.query.jpeg;req.params.grayscale=req.query.bw!=0;req.params.quality=parseInt(req.query.l,10)||DEFAULT_QUALITY;next()}
